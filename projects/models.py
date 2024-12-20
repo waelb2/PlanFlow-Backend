b8cb49a8e19e480585ef2733c29435bbe5a56f6a
waelb2 <lw_bouguessa@esi.dz>
2024-12-20 20:29:29 +0100

    adding projects app and models

---
+++ projects/models.py
@@ -0,0 +1,47 @@
+from django.conf import settings
+from django.db import models
+
+
+class Project(models.Model):
+    # Enums
+    HIGH = "High"
+    MEDIUM = "Medium"
+    LOW = "Low"
+
+    PRIORITY_CHOICES = [
+        (HIGH, "High"),
+        (MEDIUM, "Medium"),
+        (LOW, "Low"),
+    ]
+
+    NOT_STARTED = "Not Started"
+    IN_PROGRESS = "In Progress"
+    COMPLETED = "Completed"
+
+    STATUS_CHOICES = [
+        (NOT_STARTED, "Not Started"),
+        (IN_PROGRESS, "In Progress"),
+        (COMPLETED, "Completed"),
+    ]
+
+    # Fields
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default=MEDIUM)
+    category = models.CharField(max_length=100)
+    status = models.CharField(
+        max_length=15, choices=STATUS_CHOICES, default=NOT_STARTED
+    )
+    image_urls = models.JSONField(default=list, blank=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="projects"
+    )
+
+    # Keeping the tracking of creation
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title

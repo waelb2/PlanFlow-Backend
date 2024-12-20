5404911fbd518a298fd72c48bd36d9c53f737ae3
waelb2 <lw_bouguessa@esi.dz>
2024-12-20 20:30:20 +0100

    adding login urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import SignupView
+from .views import LoginView, SignupView
 
 urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
